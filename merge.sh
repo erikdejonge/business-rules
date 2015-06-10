e761f8391d4a476d5baf418b9e425acf96d505be
Erik de Jonge <erik@a8.nl>
2015-06-10 18:46:06 +0200

    Dante Alighieri: Nature is the art of God. Wednesday 10 June 2015 (week:23 day:161), 18:46:05

--- merge.sh
+++ merge.sh
@@ -2,5 +2,5 @@
 #git remote add upstream git remote add upstream git@github.com:venmo/business-rules.git
 git fetch upstream
 git checkout master
-git merge upstream/master
+git merge upstream/master -m "-"
 
