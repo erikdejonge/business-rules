8f3b7c3dc898d29d9e11de81c5d633c8b6b50582
Erik de Jonge <erik@a8.nl>
2015-06-30 16:12:39 +0200

    documentation:Tuesday 30 June 2015 (week:26 day:181), 16:12:36

--- merge.sh
+++ merge.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#git remote add upstream git remote add upstream git@github.com:venmo/business-rules.git
+#git remote add upstream git@github.com:venmo/business-rules.git
 git fetch upstream
 git checkout master
 git merge upstream/master -m "-"
